073bb253ee698617e0a3a6329e4156dd3a500470
mohuani <wfk2975019671@sina.com>
2022-02-24 18:01:49 +0800

    init
    init

---
+++ imooc-ad-service/ad-commom/src/main/java/com/imooc/ad/advice/CommonResponseDataAdvice.java
@@ -0,0 +1,34 @@
+package com.imooc.ad.advice;
+
+
+import com.imooc.ad.annotation.IgnoreResponseAdvice;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+@RestControllerAdvice
+public class CommonResponseDataAdvice implements ResponseBodyAdvice<Object> {
+
+    @Override
+    @SuppressWarnings("all")
+    public boolean supports(MethodParameter methodParameter, Class<? extends HttpMessageConverter<?>> aClass) {
+        if (methodParameter.getDeclaringClass().isAnnotationPresent(IgnoreResponseAdvice.class)) {
+            return false;
+        }
+
+        if (methodParameter.getMethod().isAnnotationPresent(IgnoreResponseAdvice.class)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public Object beforeBodyWrite(Object o, MethodParameter methodParameter, MediaType mediaType, Class<? extends HttpMessageConverter<?>> aClass, ServerHttpRequest serverHttpRequest, ServerHttpResponse serverHttpResponse) {
+        return null;
+    }
+}
